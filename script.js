ba99065b22bb4e9309c59bfed73a70b1d8d9e2b4
Sadia <Esadia@hotmail.fr>
2024-10-31 11:31:33 +0100

    codage terminé du caroussel

--- script.js
+++ script.js
@@ -3,52 +3,96 @@ async function Caroussel() {
    const reponse = await fetch("/datas/images-caroussel.json");
    const images = await reponse.json();
 
+   // Fonction pour gérer le clic sur la flèche gauche du caroussel
+   function CarousselFlecheGauche(baliseArrowLeft, baliseImg, liste, total) {
+      baliseArrowLeft.addEventListener("click", function (event) {
+         event.preventDefault();
+         let imageActuelle = baliseImg.src;
+
+         liste.pictures.map((element, index) => {
+            element === imageActuelle
+               ? imageActuelle !== liste.pictures[0]
+                  ? baliseImg.setAttribute("src", liste.pictures[index - 1])
+                  : baliseImg.setAttribute("src", liste.pictures[total])
+               : null;
+         });
+      });
+   }
+
+   // Fonction pour gérer le clic sur la flèche gauche du caroussel
+   function CarousselFlecheDroite(baliseArrowRight, baliseImg, liste, total) {
+      baliseArrowRight.addEventListener("click", function (event) {
+         event.preventDefault();
+         let imageActuelle = baliseImg.src;
+
+         liste.pictures.map((element, index) => {
+            element === imageActuelle
+               ? imageActuelle !== liste.pictures[total]
+                  ? baliseImg.setAttribute("src", liste.pictures[index + 1])
+                  : baliseImg.setAttribute("src", liste.pictures[0])
+               : null;
+         });
+      });
+   }
+
+   // PROJET 1 : KASA
+
    // Objet Kasa
    const listeKasa = images.find((i) => i.title == "Kasa");
+
+   // Affichage de la première img dans le caroussel du P1
+   const baliseImgKasa = document.querySelector(".p1__caroussel__link__img ");
+   baliseImgKasa.setAttribute("src", listeKasa.pictures[0]);
+
+   // Variable du total d'images
+   const totalKasa = listeKasa.pictures.length - 1;
+
+   // Flèche Gauche du P1
+   const baliseArrowLeftKasa = document.querySelector(".p1__arrow-left");
+   CarousselFlecheGauche(baliseArrowLeftKasa, baliseImgKasa, listeKasa, totalKasa);
+
+   // Flèche Droite du P1
+   const baliseArrowRightKasa = document.querySelector(".p1__arrow-right");
+   CarousselFlecheDroite(baliseArrowRightKasa, baliseImgKasa, listeKasa, totalKasa);
+
+   // PROJET 2 : Piiquante
 
    // Objet Piiquante
    const listePiiquante = images.find((i) => i.title == "Piiquante");
 
+   // Affichage de la première img dans le caroussel du P2
+   const baliseImgPiiquante = document.querySelector(".p2__caroussel__link__img ");
+   baliseImgPiiquante.setAttribute("src", listePiiquante.pictures[0]);
+
+   // Variables du total d'images
+   const totalPiiquante = listePiiquante.pictures.length - 1;
+
+   // Flèche Gauche du P2
+   const baliseArrowLeftPiiquante = document.querySelector(".p2__arrow-left");
+   CarousselFlecheGauche(baliseArrowLeftPiiquante, baliseImgPiiquante, listePiiquante, totalPiiquante);
+
+   // Flèche Droite du P2
+   const baliseArrowRightPiiquante = document.querySelector(".p2__arrow-right");
+   CarousselFlecheDroite(baliseArrowRightPiiquante, baliseImgPiiquante, listePiiquante, totalPiiquante);
+
+   // PROJET 3 : KANAP
+
    // Objet Kanap
    const listeKanap = images.find((i) => i.title == "Kanap");
 
-   // Affichage de la première img dans le caroussel du P1
-   let baliseImg = document.querySelector(".p1__caroussel__link__img ");
-   baliseImg.setAttribute("src", listeKasa.pictures[0]);
+   // Affichage de la première img dans le caroussel du P3
+   const baliseImgKanap = document.querySelector(".p3__caroussel__link__img ");
+   baliseImgKanap.setAttribute("src", listeKanap.pictures[0]);
 
    // Variables du total d'images
-   const totalKasa = listeKasa.pictures.length - 1;
-   const totalPiiqaunte = listePiiquante.pictures.length - 1;
    const totalKanap = listeKanap.pictures.length - 1;
 
-   // Flèche Gauche du P1
-   let baliseArrowLeft = document.querySelector(".p1__arrow-left");
-   baliseArrowLeft.addEventListener("click", function (event) {
-      event.preventDefault();
-      let imageActuelle = baliseImg.src;
+   // Flèche Gauche du P3
+   const baliseArrowLeftKanap = document.querySelector(".p3__arrow-left");
+   CarousselFlecheGauche(baliseArrowLeftKanap, baliseImgKanap, listeKanap, totalKanap);
 
-      listeKasa.pictures.map((element, index) => {
-         element === imageActuelle
-            ? imageActuelle !== listeKasa.pictures[0]
-               ? baliseImg.setAttribute("src", listeKasa.pictures[index - 1])
-               : baliseImg.setAttribute("src", listeKasa.pictures[totalKasa])
-            : null;
-      });
-   });
-
-   // Flèche Droite du P1
-   let baliseArrowRight = document.querySelector(".p1__arrow-right");
-   baliseArrowRight.addEventListener("click", function (event) {
-      event.preventDefault();
-      let imageActuelle = baliseImg.src;
-
-      listeKasa.pictures.map((element, index) => {
-         element === imageActuelle
-            ? imageActuelle !== listeKasa.pictures[totalKasa]
-               ? baliseImg.setAttribute("src", listeKasa.pictures[index + 1])
-               : baliseImg.setAttribute("src", listeKasa.pictures[0])
-            : null;
-      });
-   });
+   // Flèche Droite du P3
+   const baliseArrowRightKanap = document.querySelector(".p3__arrow-right");
+   CarousselFlecheDroite(baliseArrowRightKanap, baliseImgKanap, listeKanap, totalKanap);
 }
 Caroussel();
